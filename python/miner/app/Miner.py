6a25650e19161bfb67f51a9ad1400fcfa5907208
heyokajan <jankoot@beards.technology>
2017-02-25 19:24:38 +0100

    Bug fix: access config when script is called from crontab

--- python/miner/app/Miner.py
+++ python/miner/app/Miner.py
@@ -15,7 +15,7 @@ import configparser
 Config = configparser.ConfigParser()
 #Config.read('../config/example.ini')
 #Config.read('../config/dev.ini')
-Config.read('../config/test.ini')
+Config.read('/home/jan/eve.beards.technology/python/miner/config/test.ini')
 #Config.read('../config/prod.ini')
 
 logging.basicConfig(filename=Config.get('Logging', 'logLocation'),level=logging.INFO)
